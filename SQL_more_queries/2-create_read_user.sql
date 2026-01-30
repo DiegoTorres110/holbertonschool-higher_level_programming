6bbcef9fb8d407127e35171ba449d4d00ba5f445
DiegoTorres110 <towers9090@gmail.com>
2026-01-29 23:07:55 -0600

    Fix syntax for CREATE DATABASE statement

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,5 @@
 -- Ex 3
-CREATE DATABASE 'hbtn_0d_2'
+CREATE DATABASE hbtn_0d_2
 CREATE USER 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
