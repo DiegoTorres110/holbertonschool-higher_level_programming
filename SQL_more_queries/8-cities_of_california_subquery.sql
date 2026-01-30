8c08bc78e1cf786fe72f437ba581fbc5486bf0c4
DiegoTorres110 <towers9090@gmail.com>
2026-01-30 00:11:05 -0600

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,12 @@
 -- Ex 9
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+SELECT id, name
+FROM cities
+WHERE state_id = (
+  SELECT id
+  FROM states
+  WHERE name = 'California'
+)
+ORDER BY id;
+
+
+
