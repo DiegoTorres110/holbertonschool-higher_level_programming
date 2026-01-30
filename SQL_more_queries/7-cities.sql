7839de90c0c779d5de42886245ac4ecf1bd1098a
DiegoTorres110 <towers9090@gmail.com>
2026-01-29 23:48:37 -0600

    Add hbtn_0d_usa database and cities table
    Create database and cities table with foreign key.

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Ex 8
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  state_id INT NOT NULL,
+  FOREING KEY (state_id) REFERENCES (id)
+  name VARCHAR(256) NOT NULL
+);
