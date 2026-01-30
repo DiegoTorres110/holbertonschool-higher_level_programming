d19258dda4060ced49c675ec313c342cc2d64e3b
DiegoTorres110 <towers9090@gmail.com>
2026-01-29 23:11:49 -0600

    Modify user creation to check existence before creation

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- Ex 3
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
+
